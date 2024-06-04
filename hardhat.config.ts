e1ca5bfeb76a2721a0c5ce1a1322d40b69c723b5
dalewong008 <stephens.druej@gmail.com>
2024-06-04 16:56:24 +1000

    fix ethers provider issue

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,25 @@
-import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-web3";
+import { task } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-web3';
 
-require('dotenv').config()
-
+require('dotenv').config();
 
 export default {
   solidity: {
-    version: "0.8.4",
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   networks: {
     kovan: {
-        gas: "auto",
-        gasPrice: "auto",
-        url: process.env.API_KEY,
-        accounts: [`0x${process.env.PRIVATE_KEY}`]
-    }
-  }
-}
+      gas: 'auto',
+      gasPrice: 'auto',
+      url: process.env.API_KEY,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+};
